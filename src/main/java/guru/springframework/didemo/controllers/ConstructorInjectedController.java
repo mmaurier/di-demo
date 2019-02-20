a528616fcc8df2c8d3654f0d88137eebb378e188
marco <mmaurier1@gmail.com>
2019-02-19 22:18:27 -0500

    dependency injection #39

--- src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -1,15 +1,22 @@
 package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+
+@Controller
 public class ConstructorInjectedController {
+
     private GreetingService greetingService;
 
+
     public ConstructorInjectedController(GreetingService greetingService) {
+
         this.greetingService = greetingService;
     }
 
     public  String sayHello() {
+
         return greetingService.sayGreeting();
     }
 }
